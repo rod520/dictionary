5b273d0af01f5af706f5220fa0333227d1caafad
rod520 <rodriguesorlen@gmail.com>
2026-07-14 20:11:47 -0500

    search working, but not all the way there yet, some words are missing

---
+++ src/lib/cmpnts/Search.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+
+/* eslint-disable svelte/require-each-key */
+
+/* eslint-disable svelte/no-navigation-without-resolve */
+
+import type { FuseResult } from 'fuse.js';
+    import { fuse } from '$lib/fuse';
+    let results: FuseResult<{ keyword: string; path: string; konkani_word: string; }>[] | null = $state(null);
+    let search = () => {
+        results = fuse.search(searchInput);
+    }
+    let searchInput = $state('');
+</script>
+
+<input bind:value={searchInput} on:input={search} placeholder="Search..." />
+{#if results}
+    <ul>
+        {#each results as result}
+            <a href={result.item.path}><li>{result.item.konkani_word}</li></a>
+        {/each}
+    </ul>
+{/if}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,23 @@
-<script>
+<script lang="ts">
+/* eslint-disable svelte/require-each-key */
+
     import { entries } from '$lib/import';
     console.log(entries);
     import { searchEntries } from '$lib/fuse'
+	import Search from '$lib/cmpnts/Search.svelte';
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-{#each searchEntries as e, i }
-<br/>
-{i}
-    {JSON.stringify(e, null, 2)}
-{/each}
+<Search />
+<!--
+    {#each searchEntries as e, i }
+    <br/>
+    {i}
+        {JSON.stringify(e, null, 2)}
+    {/each}
+-->
+
 
 {#each entries as e, i }
 <br/>
