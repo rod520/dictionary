715018f14c22ffe625434f6f111bb705e2a287bd
rod520 <rodriguesorlen@gmail.com>
2026-07-15 09:47:10 -0500

    fixed the .word.json problem

--- src/lib/import.ts
+++ src/lib/import.ts
@@ -1,18 +1,21 @@
 export const entriesImport
- = import.meta.glob
-('/entries/**/*.json', { eager: true, import: 'default' })
- const entriesArray = Object.values(entriesImport)
- const entriesPath = Object.keys(entriesImport)
+    = import.meta.glob
+        ('/entries/**/*.json', { eager: true, import: 'default' })
+const entriesArray = Object.values(entriesImport)
+const entriesPath = Object.keys(entriesImport)
 
-export const entries = Object.values(entriesArray).map((entry, index) => { 
-      entry.keywords = entry.keywords.map((keyword) => keyword.toLowerCase())
-
-entry.path = entriesPath[index].replace('.json', '').replace(/^\/?entries\/(\w+)\/(\w+)$/, '/entries/$1-$2')
-console.log('entry.path:', entry.path)
+export const entries = Object.values(entriesArray).map((entry, index) => {
+    //@ts-expect-error thats fine
+    entry.keywords = entry.keywords.map((keyword) => keyword.toLowerCase())
+    // the first replace removes the .json extension, the second replace changes the path to a more readable format, and the third replace changes leading underscores to periods
+    //@ts-expect-error again, fine
+    entry.path = entriesPath[index].replace('.json', '').replace(/^\/?entries\/(\w+)\/(\w+)$/, '/entries/$1-$2').replace(/_(\w+)\.json/g, '.$1.json')
     return entry
 
-})
+}) //@ts-expect-error again, fine
     .filter((entry) => entry.status === 'published') as Entry[]
+
+    // the type of our entries
 type Entry = {
     konkani_word: string;
     meaning: string[];
